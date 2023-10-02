9261909a6544f975d411bec92db303eabf01d0ea
yukinoohata <sunstone.neige11@gmail.com>
2023-10-02 13:59:44 +0900

    16章

--- src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -95,5 +95,14 @@ public class AdminHouseController {
 
         return "redirect:/admin/houses";
     }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable(name = "id") Integer id, RedirectAttributes redirectAttributes) {
+        houseRepository.deleteById(id);
+
+        redirectAttributes.addFlashAttribute("successMessage", "民宿を削除しました。");
+
+        return "redirect:/admin/houses";
+    }
 }
 
