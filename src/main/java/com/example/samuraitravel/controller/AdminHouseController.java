d6f26d2523cf13a4e2d9c34e8d79888fecafd67b
yukinoohata <sunstone.neige11@gmail.com>
2023-10-02 10:18:49 +0900

    ９章

--- src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -5,15 +5,14 @@ import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.repository.HouseRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -26,9 +25,17 @@ public class AdminHouseController {
     }
 
     @GetMapping
-    public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
-        Page<House> housePage = houseRepository.findAll(pageable);
+    public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, @RequestParam(name="keyword", required = false)String keyword) {
+        Page<House> housePage;
+
+        if (keyword != null && !keyword.isEmpty()){
+            housePage=houseRepository.findByNameLike("%"+keyword+"%", pageable);
+        }else{
+            housePage=houseRepository.findAll(pageable);
+        }
         model.addAttribute("housePage", housePage);
+        model.addAttribute("keyword", keyword);
+
         return "admin/houses/index";
 
     }
--- src/main/java/com/example/samuraitravel/repository/HouseRepository.java
+++ src/main/java/com/example/samuraitravel/repository/HouseRepository.java
@@ -1,7 +1,10 @@
 package com.example.samuraitravel.repository;
 
 import com.example.samuraitravel.entity.House;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HouseRepository extends JpaRepository<House, Integer> {
+    public Page<House> findByNameLike(String keyword, Pageable pageable);
 }
