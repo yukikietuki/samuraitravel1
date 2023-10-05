faced75efe3e32fff387e50a94ef6dcef9bd4d7f
yukinoohata <sunstone.neige11@gmail.com>
2023-10-05 15:01:17 +0900

    29章

--- src/main/java/com/example/samuraitravel/controller/HomeController.java
+++ src/main/java/com/example/samuraitravel/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.example.samuraitravel.controller;
 
-
+import java.util.List;
+import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.entity.House;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
 
 @Controller
 public class HomeController {
+    private final HouseRepository houseRepository;
+
+    public HomeController(HouseRepository houseRepository) {
+        this.houseRepository = houseRepository;
+    }
     @GetMapping("/")
-    public String index(){
+    public String index(Model model) {
+        List<House> newHouses = houseRepository.findTop10ByOrderByCreatedAtDesc();
+        model.addAttribute("newHouses", newHouses);
         return "index";
     }
 }
--- src/main/java/com/example/samuraitravel/repository/HouseRepository.java
+++ src/main/java/com/example/samuraitravel/repository/HouseRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HouseRepository extends JpaRepository<House, Integer> {
     public Page<House> findByNameLike(String keyword, Pageable pageable);
     public Page<House> findByNameLikeOrAddressLikeOrderByCreatedAtDesc(String nameKeyword, String addressKeyword, Pageable pageable);
@@ -15,5 +17,7 @@ public interface HouseRepository extends JpaRepository<House, Integer> {
     public Page<House> findByPriceLessThanEqualOrderByPriceAsc(Integer price, Pageable pageable);
     public Page<House> findAllByOrderByCreatedAtDesc(Pageable pageable);
     public Page<House> findAllByOrderByPriceAsc(Pageable pageable);
+    public List<House> findTop10ByOrderByCreatedAtDesc();
+
 }
 
